aaa6895ecb98f41d34f7e52f7411abfab2d86c85
Radeleon <rudy209@comcast.net>
2019-07-17 11:20:28 -0700

    updated algo to work properly

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -3,20 +3,65 @@ start = time.time()
 
 
 
-# TO-DO: complete the helpe function below to merge 2 sorted arrays
+# TO-DO: complete the helper function below to merge 2 sorted arrays
 def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
     merged_arr = [0] * elements
+ #Arrays already sorted! no need < JK!
+  # merged_arr = list(set(arrA + arrB))
+    i = 0
+    j = 0
+    idx = 0
     # TO-DO
-    
+    while i < len(arrA) and j < len(arrB):
+        if arrA[i] < arrB[j]:
+            merged_arr[idx] = arrA[i]
+            i += 1
+        else:
+            merged_arr[idx] =  arrB[j]
+            j += 1
+        index += 1
+    while i < len(arrA):
+        merged_arr[index] = arrA[i]
+        i += 1
+        index += 1
+    while j < len(arrB):
+        merged_arr[index] = arrB[j]
+        j += 1
+        index += 1
+
+ 
+    print(merged_arr)
+
     return merged_arr
 
+
+arr1 = [0,1,2,3,7,4,8,9,2,3,1,2,0]
+arr2 = [4,5,6,1,1,1]
+
+merge(arr1,arr2)
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
     # TO-DO
+    # if greater than 1 lets split it otherwise no need to merge and sort 1 value
+    if len(arr) > 1:
+        # return arr
+        #split into individual arr?
+        # split into 2 arr and sort
+        else:
+            middle = int(len(arr) //2) #pivor?  
+            left_arr = merge_sort(arr[:middle])
+            right_arr = merge_sort(arr[middle:])
+        # merge into arr of 2 and with smallest of the 2 num first
+            return merge(left,right)
 
     return arr
+
+
+
+
+
 
 
 
